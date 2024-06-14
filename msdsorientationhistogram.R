55829b909cf84d63a980c949c5decb2530ff1865
Farah Turkistani <ft8xx@virginia.edu>
2024-06-14 16:03:32 -0400

    commiting initialdata

---
+++ msdsorientationhistogram.R
@@ -0,0 +1,5 @@
+hist(MSDS_Orientation_Computer_Survey$`CPU Number of Cores (int)`,
+     main = "CPU Cores in MSDS Residential Students' Laptops",
+     xlab = "Number of CPU Cores",
+     ylab = "Number of Students",
+     col = "sky blue")
